8e6ad399b208a1802f105e7c1f6fe7701e4e2bc7
kares <self@kares.org>
2021-05-31 14:14:39 +0200

    CI: keep re-using base docker-compose.yml

--- .ci/docker-run.sh
+++ .ci/docker-run.sh
@@ -5,6 +5,5 @@ set -ex
 
 # TEST_MODE should be one of "unit" or "integration" (defaults to "unit" unless INTEGRATION=true)
 : "${TEST_MODE:=$([[ "${INTEGRATION}" = "true" ]] && echo "integration" || echo "unit")}"
-export TEST_MODE
 
-docker-compose -f ".ci/common/docker-compose.yml" -f ".ci/${TEST_MODE}/docker-compose.override.yml" up --exit-code-from logstash
+docker-compose -f ".ci/docker-compose.yml" -f ".ci/${TEST_MODE}/docker-compose.override.yml" up --exit-code-from logstash
